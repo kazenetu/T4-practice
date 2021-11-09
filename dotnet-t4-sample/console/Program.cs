76ec125dc8d36e5c09db95223a401fe770f0f240
kazenetu <rakudafactory@gmail.com>
2021-11-09 22:50:25 +0900

    余計なファイルが出力される処理を削除 close #2

--- dotnet-t4-sample/console/Program.cs
+++ dotnet-t4-sample/console/Program.cs
@@ -11,76 +11,9 @@ namespace t4_practice
             var data = new ClassInfo() { ClassName = "test" };
             ITransformText page = new test1_cs(data);
             var pageContent = page.TransformText();
-            System.IO.File.WriteAllText($"{nameof(test1_cs)}.TransformText.txt", ReadT4("test1_cs", nameof(t4_practice)));
 
             Console.WriteLine(pageContent);
-            Console.WriteLine();
-            Console.WriteLine(ReadT4("test1_cs", nameof(t4_practice)));
         }
 
-        static string ReadT4(string templateName, string nameSpace)
-        {
-            var templateFile = RemoveParams(System.IO.File.ReadAllText($"{templateName}.tt"));
-            return CreateTransformText(templateFile);
-
-            string RemoveParams(string src)
-            {
-                src = GetRegex(src, $"<#@ template (.+?) #>{Environment.NewLine}", string.Empty);
-                src = GetRegex(src, $"<#@ assembly (.+?) #>{Environment.NewLine}", string.Empty);
-                src = GetRegex(src, $"<#@ output (.+?) #>{Environment.NewLine}", string.Empty);
-                return src;
-            }
-
-            // using
-            string ConvertUsings(string src)
-            {
-                return GetRegex(src, "<#@ import namespace=\"(.+?)\" #>", "using $1;");
-            }
-
-            // params
-            string ConvertParams(string src)
-            {
-                return GetRegex(src, "<#= (.+?) #>", "{$1}");
-            }
-
-            // 最終出力
-            string CreateTransformText(string src)
-            {
-                var result = new StringBuilder();
-                result.AppendLine("using System.Text;");
-                result.AppendLine($"namespace {nameSpace}");
-                result.AppendLine("{");
-                result.AppendLine($"  public partial class {templateName}");
-                result.AppendLine("   {");
-                result.AppendLine("     public string TransformText()");
-                result.AppendLine("     {");
-
-                result.AppendLine("       var template = new StringBuilder();");
-
-                var rgx = new Regex("({|})");
-                var templates = ConvertParams(ConvertUsings(templateFile)).Split(Environment.NewLine);
-                foreach (var line in templates)
-                {
-                    var outputLine = line;
-                    if(rgx.Matches(outputLine).Count < 2)
-                    {
-                        outputLine = outputLine.Replace("{","{{").Replace("}","}}");
-                    }
-                    result.AppendLine($"       template.AppendLine($\"{outputLine}\");");
-                }
-
-                result.AppendLine("       return template.ToString();");
-                result.AppendLine("     }");
-                result.AppendLine("   }");
-                result.AppendLine("}");
-
-                return result.ToString();
-            }
-
-            string GetRegex(string src, string reg, string replace)
-            {
-                return Regex.Replace(src, reg, replace);
-            }
-        }
     }
 }
